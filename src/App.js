a9eefacf2f7dc4abcf97e8cdd29d945b53e2fbea
Jan Mager <jasiu.mager@gmail.com>
2022-11-23 19:06:57 +0100

    big update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,153 @@
 import './App.css';
+import styled from 'styled-components';
 
 import Nav from './components/Nav/Nav';
+import { useState } from 'react';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  background: gray;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+
+  > div > div{
+    padding: 25px;
+  }
+`
+
+const SwitchModeBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  color: #39375B;
+  background: #FF7B9C;
+
+  div{
+    width: 50%;
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    font-weight: 600;
+
+    &:hover{
+      background: red;
+      color: rebeccapurple;
+    }
+  }
+`
+
+const InputOutputBox = styled.div`
+    height: 100%;
+    background: #39375B;
+    color: #B3C2F2;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 175%;
+
+  > div:nth-of-type(2){
+    background: #B3C2F2;
+    color: #39375B;
+  }
+  textarea{
+    display: flex;
+    width: 100%;
+    padding: 15px;
+    box-sizing: border-box;
+    resize: none;
+    height: 50%;
+    background: #39375B;
+    color: #B3C2F2;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 130%;
+    font-family: inherit;
+
+    &::placeholder{
+      opacity: .2;
+      font-size: 22px;
+      color: inherit;
+    }
+  }
+
+  textarea:nth-of-type(2){
+    background: #B3C2F2;
+    color: #39375B;
+
+    &::placeholder{
+      opacity: .2;
+    }
+  }
+`
+
+const SwtichModeButton = styled.span`
+    height: 50px;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    font-weight: 400;
+    font-size: 14px;
+    opacity: .5;
+    transition: .2s;
+
+    &.active{
+      font-weight: 600;
+      opacity: 1;
+      font-size: 18px;
+    }
+
+    &:hover{
+      cursor: pointer;
+      font-weight: 600;
+      opacity: .75;
+      font-size: 18px;
+      transition: .2s;
+    }
+`
 
 function App() {
+  // 0 szyfruj
+  // 1 deszyfruj
+  const [ mode, setMode ] = useState(0)
+
+  const inputPlaceholders = [
+    'Wpisz tekst, który chcesz zaszyfrować',
+    'Wpisz tekst, który chcesz odszyfrować'
+  ]
+
+  const outputPlaceholders = [
+    'Wpisz teskt powyżej',
+    'Wpisz teskt powyżej'
+  ]
+  
   return (
-    <div className="App">
-      <Nav />
+    <Container>
       <div>
-        działanie kodowanie/rozkodowanie
+        <Nav />
+        <SwitchModeBox>
+          <SwtichModeButton className={mode == 0 ? 'active' : null} onClick={() => setMode(0)}>
+            szyfruj
+          </SwtichModeButton>
+          <SwtichModeButton className={mode == 1 ? 'active' : null} onClick={() => setMode(1)}>
+            deszyfruj
+          </SwtichModeButton>
+        </SwitchModeBox>
       </div>
-      <div>
-        <div>
-          input
-        </div>
-        <div>
-          output
-        </div>
-      </div>
-    </div>
+      <InputOutputBox>
+        <textarea placeholder={inputPlaceholders[mode]}></textarea>
+        <textarea disabled placeholder={outputPlaceholders[mode]}></textarea>
+      </InputOutputBox>
+    </Container>
   );
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,166 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import styled from 'styled-components'
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  flex-direction: column;
+  color: #FF7B9C;
+  background: #39375B;
+  align-items: flex-start;
+  gap: 10px;
+
+  div{
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+  }
+
+  div p{
+    display: flex;
+    flex-direction: row;
+  }
+
+  div b{
+    margin-left: 5px;
+  }
+`
+
+const InfoButton = styled.div`
+  cursor: pointer;
+  margin-left: 15px;
+  transition: .2s;
+
+  &:hover{
+    transform: scale(2);
+    transition: .2s;
+    opacity: 1;
+  }
+`
+
+const AlgoInfoModal = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0,0,0,0.75);
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  color: white;
+
+  h1{
+    font-size: 32px;
+    text-align: center;
+  }
+
+  p{
+    max-width: 550px;
+    font-size: 20px;
+    margin-top: 45px;
+    line-height: 150%;
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  table{
+    margin-top: 25px;
+    border-collapse: collapse;
+  }
+
+  table tr:nth-of-type(1) td:nth-of-type(1), table tr:nth-of-type(1) td, table tr td:nth-of-type(1){
+    background: #FF7B9C;
+    font-size: 16px;
+    color: #39375B;
+  }
+
+  table td{
+    font-size: 22px;
+    text-align: center;
+    font-weight: bold;
+    width: 40px;
+    height: 40px;
+    border: 1px solid #FF7B9C;
+    border-collapse: collapse;
+    padding: 10px;
+  }
+`
+
 
 function Nav() {
+
+const [ showInfo, setShowInfo ] = useState(false)
+
   return (
-    <div style={{display: 'flex', justifyContent: 'flex-start', width: '100%', flexDirection: 'column', background: 'white', alignItems: 'flex-start', gap: '10px'}}>
-        <div style={{gap: '10px', display: 'flex', flexDirection: 'column'}}>
+    <NavContainer>
+        {showInfo && <AlgoInfoModal onClick={() => setShowInfo(false)}>
+          <h1>Szachownica Polibiusza</h1>
+          <p>Szyfr został opracowany przez greckiego historyka Polibiusza. Litery szyfrujemy zgodnie z poniższą szachownicą, najpierw numer kolumny, potem wiersza np.:</p>
+          <table>
+            <tr>
+              <td></td>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>A</td>
+              <td>B</td>
+              <td>C</td>
+              <td>D</td>
+              <td>E</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>F</td>
+              <td>G</td>
+              <td>H</td>
+              <td style={{letterSpacing: '2px'}}>I/J</td>
+              <td>K</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>L</td>
+              <td>M</td>
+              <td>N</td>
+              <td>O</td>
+              <td>P</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Q</td>
+              <td>R</td>
+              <td>S</td>
+              <td>T</td>
+              <td>U</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>V</td>
+              <td>W</td>
+              <td>X</td>
+              <td>Y</td>
+              <td>Z</td>
+            </tr>
+          </table>
+        </AlgoInfoModal>
+        }
+        <div>
             <h1>Kryptograficzne szyfrowanie tekstu</h1>
-            <p>Jan Mager, numer albumu: <b>120117</b>, grupa <b>K23</b></p>
+            <p><b style={{marginLeft: '0px'}}>Jan Mager</b>, numer albumu: <b>120117</b>, grupa <b> K23</b></p>
         </div>
-        <div style={{gap: '10px', display: 'flex', flexDirection: 'column'}}>
-            <p>Wykorzystany algorytm: <b>SZYFR CEZARA</b></p>
-            <p>Działanie: <b>lorem ipsum lorem ipsun porem psu</b></p>
+        <div>
+            <p>Metoda szyfrowania: <b>SZACHOWNICA POLIBIUSZA</b> <InfoButton onClick={() => setShowInfo(true)}>ℹ️</InfoButton></p>
         </div>
-    </div>
+    </NavContainer>
   )
 }
 
